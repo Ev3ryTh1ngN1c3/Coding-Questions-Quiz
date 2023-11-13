eddba77dc0596e82e1526ed44b7b3baf051fc7c7
Lisa <143395934+Ev3ryTh1ngN1c3@users.noreply.github.com>
2023-11-13 15:09:59 -0500

    starting over

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-let timeLeft = document.querySelector(".time-left");
-let quizContainer = document.getElementById("container");
-let next8tn = document.getElementById("next-button");
-let countOfQuestion = document.querySelector("*.numer-of-question");
-let displayContainer = document.getElementById("display-container");
-let scoreContainer = document.querySelector(".score-container");
-let restart = document.getElementById("restart");
-let userScore = document.getElementById("user-score");
-let startScreen = document.querySelector("start-screen");
-let startButton = document.getElementById("start-button");
-let questionCount;
-let scoreCount = 0;
-let count = 11;
-let countDown;
+
 
 //questions & answers
 const quizArray = [
@@ -30,77 +17,8 @@ const quizArray = [
     {
         id: "2",
         question: "In JavaScript, what is a block of code called that is used to perform a specific task?",
-        choices: ["variable", "string", "declaration", "function"],
+        options: ["variable", "string", "declaration", "function"],
         correct: "function",
     },
 ];
 
-restart.addEventListener("click", () => {
-    initial();
-    displayContainer.classList.remove("hide");
-    scoreContainer.classList.add("hide");
-});
-
-next8tn.addEventListener("click", (displayNext = () => {
-    questionCount += 1;
-
-    if (questionCount == quizArray.length) {
-        displayContainer.classList.add("hide");
-        scoreContainer.classList.remove("hide");
-        userScore.innerHTML = "score is" + scoreCount + "out of" + "questionCount";
-    }
-    else {
-        countOfQuestion.innerHTML = questionCount + 1 + "of" + quizArray.length + "question";
-
-        quizDisplay(questionCount);
-        count = 4;
-        clearInterval(countDown);
-        timerDisplay();
-    }
-})
-);
-
-const TimerDisplay = () => {
-    countDown = setInterval(() => {
-        count--;
-        timeLeft.innerHTML = '$(count)s';
-        if (count == 0) {
-            clearInterval(countdown);
-            displayNext();
-        }
-    }, 1000);
-};
-
-const quizDisplay = (questionCount) => {
-    let quizCards = document.querySelectorAll(".container-mid");
-
-    quizCards.forEach((card) => {
-        card.classList.add("hide");
-    });
-    quizCards[questionCount].classList.remove("hide");
-};
-
-function quizContainer() {
-    quizArray.sort(() => Math.random() - 0.5);
-
-    for (let i of quizArray) {
-        i.options.sort(() => Math.random() - 0.5);
-        let div = document.createElement("div");
-        div.classList.add("container-mid", "hide");
-
-        countOfQuestion.innerHTML = 1 + "of" + quizArray.length + "question";
-
-        let questiion_DIV = document.createElement("p");
-        question_DIV.classList.add("question");
-        question_DIV.innerHTML = i.question;
-        div.appendChild(question_DIV);
-
-        div.innerHTML += 
-        <button class="option-div" onclick="checker(this)"> 
-        $(i.option[0]</button>;
-        <button class="option-div" onclick="checker(this)"> 
-        $(i.option[1]</button>;
-        <button class="option-div" onclick="checker(this)"> 
-        $(i.option[2]</button>
-    }
-}

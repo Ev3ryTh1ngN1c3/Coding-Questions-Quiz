87ba9a6394f4b068886717254866420c29e12276
Lisa <143395934+Ev3ryTh1ngN1c3@users.noreply.github.com>
2023-11-12 20:39:29 -0500

    fixed syntax errors

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ let count = 11;
 let countDown;
 
 //questions & answers
-const quizArray = {
+const quizArray = [
 {
     id: "0",
         question: "What tag specifies a section of text in HTML?",
@@ -22,15 +22,15 @@ const quizArray = {
                 correct: "<p>",
 },
 {
-    id: "1"
+    id: "1",
     question: "What is the most important CSS property, used for controlling the layout ?",
         options: ["display", "margin", "table", "<div>"],
             correct: "display",
 },
 {
-    id: "2"
+    id: "2",
     question: "In JavaScript, what is a block of code called that is used to perform a specific task?",
         choices: ["variable", "string", "declaration", "function"],
             correct: "function",
 },
-};
+];

bbb59576099213cbfd652ce3a50b7c23d05d67ff
Lisa <143395934+Ev3ryTh1ngN1c3@users.noreply.github.com>
2023-11-12 20:19:33 -0500

    added questions, options & correct answer

--- script.js
+++ script.js
@@ -15,20 +15,22 @@ let countDown;
 
 //questions & answers
 const quizArray = {
+{
     id: "0",
-    question: "What tag specifies a section of text in HTML?",
-    options: {"<code>", "<caption>", "<p>", "<section>"},
-    correct: "<p>"
-}
+        question: "What tag specifies a section of text in HTML?",
+            options: ["<code>", "<caption>", "<p>", "<section>"],
+                correct: "<p>",
+},
 {
     id: "1"
     question: "What is the most important CSS property, used for controlling the layout ?",
-    options: {"display", "margin", "table", "<div>"},
-    correctAnswer: "display"
-}
+        options: ["display", "margin", "table", "<div>"],
+            correct: "display",
+},
 {
     id: "2"
     question: "In JavaScript, what is a block of code called that is used to perform a specific task?",
-    choices: {"variable", "string", "declaration", "function"},
-    correctAnswer: "function"
-}
+        choices: ["variable", "string", "declaration", "function"],
+            correct: "function",
+},
+};
